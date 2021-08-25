24a59753ca30add91f86a8a069e4b8ac1f9fb6b9
BuildTools <59115290+BanTheNons@users.noreply.github.com>
2021-08-25 21:07:04 +0300

    updated this too

--- api/src/routes/channels/#channel_id/directory-entries.ts
+++ api/src/routes/channels/#channel_id/directory-entries.ts
@@ -11,45 +11,49 @@ router.get("/", async (req: Request, res: Response) => {
     const { guild_id } = await ChannelModel.findOne({ id: req.params.channel_id });
 
     const guild = await GuildModel.findOne({ id: guild_id });
-    const guilded = await GuildModel.findOne({ id: "879416215544623174" });
 
-	res.json(
-        [
-            {
-                author_id: process.env.INSTANCE_OWNER_ID ?? "",
-                created_at: new Date(),
-                description: guild.description || "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-                directory_channel_id: req.params.channel_id,
-                entity_id: guild_id,
-                guild: {
-                    approximate_member_count: 1,
-                    approximate_presence_count: 11,
-                    features: guild.features,
-                    icon: guild.icon,
-                    name: guild.name,
-                    splash: guild.splash
-                },
-                primary_category_id: 1,
-                type: 0
+    let guilded
+    try {
+        guilded = await GuildModel.findOne({ id: "879358730968326152" })
+    } catch (e) {}
+
+    const response = [
+        {
+            author_id: process.env.INSTANCE_OWNER_ID ?? "",
+            created_at: new Date(),
+            description: guild.description || "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+            directory_channel_id: req.params.channel_id,
+            entity_id: guild_id,
+            guild: {
+                approximate_member_count: 1,
+                approximate_presence_count: 11,
+                features: guild.features,
+                icon: guild.icon,
+                name: guild.name,
+                splash: guild.splash
             },
-            {
-                author_id: process.env.INSTANCE_OWNER_ID ?? "",
-                created_at: new Date(),
-                description: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
-                directory_channel_id: req.params.channel_id,
-                entity_id: guilded.id,
-                guild: {
-                    approximate_member_count: -1337,
-                    approximate_presence_count: 1337,
-                    features: guilded.features,
-                    icon: guilded.icon,
-                    name: guilded.name,
-                    splash: guilded.splash
-                },
-                primary_category_id: 1,
-                type: 0
-            }
-        ]
+            primary_category_id: 1,
+            type: 0
+        }
+    ]
+    if (guilded) response.push(
+        {
+            author_id: process.env.INSTANCE_OWNER_ID ?? "",
+            created_at: new Date(),
+            description: "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",
+            directory_channel_id: req.params.channel_id,
+            entity_id: guilded.id,
+            guild: {
+                approximate_member_count: -1337,
+                approximate_presence_count: 1337,
+                features: guilded.features,
+                icon: guilded.icon,
+                name: guilded.name,
+                splash: guilded.splash
+            },
+            primary_category_id: 1,
+            type: 0
+        }
     )
 });
 
